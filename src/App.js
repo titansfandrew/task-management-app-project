ea47e51c2bdcb5c876a7c8eb9d7bc2f2cd5da0e3
titansfandrew <drewdulaney5@gmail.com>
2024-12-02 02:18:46 -0600

    updated app css and js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+//KNown error when using BrowserRouter on Github pages react  websites
+//import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+//HashRouter - reccomended for github pages react frontend website 
+import {HashRouter, Routes, Route} from "react-router-dom"
+import NavBar from "./components/navbar";
+import Home from "./components/home";
+import ContactUs from "./components/contactus";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <HashRouter>            
+      <NavBar/>  
+      <Routes>       
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<ContactUs/>}/>
+      </Routes>
+    </HashRouter>    
   );
+
 }
 
 export default App;

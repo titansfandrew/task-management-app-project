982326c04cdb16c578753b4750ddd0e9ae67a073
titansfandrew <drewdulaney5@gmail.com>
2024-12-02 02:34:53 -0600

    updated css

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import TodoList from "./todolist";
 function Home() {
     return (
       <>
-        <TodoList title="React TodoList App 1.2.5"/>
+        <TodoList title="React TodoList App"/>
       </>
     );
   }
